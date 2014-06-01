1a64eaeaa2efbecda1f011c79233a563a6b7f262
JesseHerrick <jessegrantherrick@gmail.com>
2014-06-01 18:29:27 -0400

    Rake task and more tests

---
+++ test/test_calculator_basic.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'calculator'
+
+class TestCalculator < Minitest::Test
+  def setup
+    @calc = Calculator.new
+  end
+
+  def test_add
+    assert_equal 4, @calc.add(2, 2)
+  end
+
+  def test_failing_add
+    refute_equal 5, @calc.add(2, 2)
+  end
+
+  def test_subtract
+    assert_equal 0, @calc.subtract(2, 2)
+  end
+
+  def test_failing_subtract
+    refute_equal -1, @calc.subtract(2, 2)
+  end
+end
