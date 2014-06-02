e9eaf79c7b7aa20cbb030a7b164734a059dbea84
JesseHerrick <jessegrantherrick@gmail.com>
2014-06-01 22:31:50 -0400

    Simplify

--- test/test_calculator_basic.rb
+++ test/test_calculator_basic.rb
@@ -10,15 +10,15 @@ class TestCalculator < Minitest::Test
     assert_equal 4, @calc.add(2, 2)
   end
 
-  def test_failing_add
-    refute_equal 5, @calc.add(2, 2)
-  end
+#  def test_failing_add
+#    refute_equal 5, @calc.add(2, 2)
+# end
 
   def test_subtract
     assert_equal 0, @calc.subtract(2, 2)
   end
 
-  def test_failing_subtract
-    refute_equal -1, @calc.subtract(2, 2)
-  end
+# def test_failing_subtract
+#   refute_equal -1, @calc.subtract(2, 2)
+# end
 end
