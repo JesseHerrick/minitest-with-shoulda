a1584311b4fa68fba3b528bb3d116718bb670c90
JesseHerrick <jessegrantherrick@gmail.com>
2014-06-01 22:57:07 -0400

    Add more to the basic calculator

--- test/test_calculator_basic.rb
+++ test/test_calculator_basic.rb
@@ -6,19 +6,21 @@ class TestCalculator < Minitest::Test
     @calc = Calculator.new
   end
 
+  # add
   def test_add
     assert_equal 4, @calc.add(2, 2)
   end
 
-#  def test_failing_add
-#    refute_equal 5, @calc.add(2, 2)
-# end
+  def test_failing_add
+     refute_equal 5, @calc.add(2, 2)
+  end
 
+  # subtract
   def test_subtract
     assert_equal 0, @calc.subtract(2, 2)
   end
 
-# def test_failing_subtract
-#   refute_equal -1, @calc.subtract(2, 2)
-# end
+  def test_failing_subtract
+    refute_equal -1, @calc.subtract(2, 2)
+  end
 end
